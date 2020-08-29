d3b59f03a2cac70a099000d357d9d4bfe3bc6cc5
MasatoMakino <unagiinu.g@gmail.com>
2020-08-29 14:56:26 +0900

    change : remove extend MouseEvent.

--- __test__/FakeMouseEvent.spec.ts
+++ __test__/FakeMouseEvent.spec.ts
@@ -1,4 +1,5 @@
 import { getMouseEvent } from "..";
+import mock = jest.mock;
 
 describe("FakeMouseEvent", () => {
   test("getMouseEvent", () => {
@@ -8,8 +9,48 @@ describe("FakeMouseEvent", () => {
   });
 
   test("getMouseEvent with option", () => {
-    const e = getMouseEvent("click", { x: 100, y: 100 });
+    const e = getMouseEvent("click", {
+      x: 100,
+      y: 100,
+      offsetX: 150,
+      offsetY: 150,
+    });
     expect(e).toBeTruthy();
     expect(e.x).toBe(100);
+    expect(e.offsetX).toBe(150);
+  });
+
+  test("getMouseEvent as mouseevent", () => {
+    const e = getMouseEvent("click", {
+      x: 100,
+      y: 100,
+      offsetX: 150,
+      offsetY: 150,
+    });
+    const mv = e as MouseEvent;
+    expect(mv.x).toBe(100);
+  });
+
+  test("canvas callback", () => {
+    const eventArgs = {
+      x: 100,
+      y: 100,
+      offsetX: 150,
+      offsetY: 150,
+      pageX: 0,
+      pageY: 0,
+    };
+    const e = getMouseEvent("click", { ...eventArgs });
+    const mockCallback = jest.fn((e) => e);
+    const canvas = document.createElement("canvas");
+    canvas.width = 640;
+    canvas.height = 480;
+    canvas.addEventListener("click", mockCallback);
+
+    canvas.dispatchEvent(e);
+    expect(mockCallback).toBeCalledTimes(1);
+    expect(mockCallback).toHaveBeenLastCalledWith(e);
+    expect(mockCallback).lastReturnedWith(e);
+    mockCallback.mockClear();
   });
 });
--- src/FakeMouseEvent.ts
+++ src/FakeMouseEvent.ts
@@ -1,16 +1,52 @@
-import {FakeMouseEventInit} from "./FakeMouseEventInit";
+import { FakeMouseEventInit } from "./FakeMouseEventInit";
 
-export class FakeMouseEvent extends MouseEvent {
+class MouseEventMock extends UIEvent {
+  altKey: boolean;
+  button: number;
+  buttons: number;
+  clientX: number;
+  clientY: number;
+  ctrlKey: boolean;
+  metaKey: boolean;
+  movementX: number;
+  movementY: number;
   offsetX: number;
   offsetY: number;
   pageX: number;
   pageY: number;
+  relatedTarget: EventTarget | null;
+  screenX: number;
+  screenY: number;
+  shiftKey: boolean;
   x: number;
   y: number;
+  public getModifierState(keyArg: string): boolean {
+    return false;
+  }
+  initMouseEvent(
+    typeArg: string,
+    canBubbleArg: boolean,
+    cancelableArg: boolean,
+    viewArg: Window,
+    detailArg: number,
+    screenXArg: number,
+    screenYArg: number,
+    clientXArg: number,
+    clientYArg: number,
+    ctrlKeyArg: boolean,
+    altKeyArg: boolean,
+    shiftKeyArg: boolean,
+    metaKeyArg: boolean,
+    buttonArg: number,
+    relatedTargetArg: EventTarget | null
+  ): void {}
 
-  constructor(type: string, values: FakeMouseEventInit) {
-    super(type, values);
-
+  constructor(type: string, eventInitDict?: EventInit) {
+    super(type, eventInitDict);
+  }
+}
+export class FakeMouseEvent extends MouseEventMock {
+  init(values: FakeMouseEventInit) {
     this.offsetX = values.offsetX;
     this.offsetY = values.offsetY;
     this.pageX = values.pageX;
@@ -19,4 +55,3 @@ export class FakeMouseEvent extends MouseEvent {
     this.y = values.y;
   }
 }
-
