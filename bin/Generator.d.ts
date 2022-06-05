be33e80cef743acd159c6b4617557b57eca38a58
MasatoMakino <makino.masato.g@gmail.com>
2022-06-05 14:35:41 +0900

    remove : bin
    tsc済みのバイナリはgitで管理しない。

--- bin/Generator.d.ts
+++
@@ -1,4 +0,0 @@
-import { FakeMouseEvent } from "./FakeMouseEvent";
-import { FakeMouseEventInit } from "./FakeMouseEventInit";
-export declare function getMouseEvent(type: string, values?: FakeMouseEventInit): FakeMouseEvent;
-//# sourceMappingURL=Generator.d.ts.map
