40cb2accfde8579c022d5f32b3a7d2242b1452dd
MasatoMakino <makino.masato.g@gmail.com>
2023-02-17 16:31:54 +0900

    update : dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   collectCoverageFrom: ["**/bin/**/*.js"],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  }
+  transform: {
+    "\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
+  },
 };
