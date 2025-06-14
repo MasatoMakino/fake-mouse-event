d65868b1c5ddb23b1455570f2d742d7217961e36
MasatoMakino <makino.masato.g@gmail.com>
2025-06-14 14:35:01 +0900

    chore: remove Jest configuration files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  collectCoverageFrom: ["./src/**/*.ts"],
-  transform: {
-    "\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
-  },
-};
